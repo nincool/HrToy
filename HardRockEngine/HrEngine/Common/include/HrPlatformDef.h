590b29bc2e0dd3c78bd30d116fda705199c51c03
nincool <howieren@163.com>
2015-02-28 20:18:08 +0800

    添加锁

--- HardRockEngine/HrEngine/Common/include/HrPlatformDef.h
+++ HardRockEngine/HrEngine/Common/include/HrPlatformDef.h
@@ -2,9 +2,6 @@
 #define _HR_PLATFORM_H_
 
 
-
-
-
 #ifndef SAFE_DELETE
 #define SAFE_DELETE(p) { if(p != NULL) { delete (p); (p)=NULL; } }
 #endif
@@ -25,18 +22,20 @@
 
 
 
-#ifdef WINDOWNS 
+#ifdef HR_WINDOWS 
 #define	VSNPRINTF(pBuff, nBufLen, nCount, pFormat, pArglist)	vsnprintf_s(pBuff, nBufLen, nCount, pFormat, pArglist)
 #define STRCAT(pBufDes, nBufLen, pBufSrc)						strcat_s(pBufDes, nBufLen, pBufSrc)
 #define LOCALTIME(tm, t)										localtime_s(tm, t)
 #define STRCPY(pBufDes, nBufLen, pBufSrc)						strcpy_s(pBufDes, nBufLen, pBufSrc)
 #define STAT										_stat
+#define FOPEN(pFile, pFileName, pModel)                         fopen_s(&pFile, pFileName, pModel)
 #else
 #define VSNPRINTF(pBuff, nBufLen, nCount, pFormat, pArglist)	vsnprintf(pBuff, nBufLen, pFormat, pArglist)
 #define STRCAT(pBufDes, nBufLen, pBufSrc)						strcat(pBufDes, pBufSrc)
 #define LOCALTIME(tm, t)										localtime_r(t, tm)
 #define STRCPY(pBufDes, nBufLen, pBufSrc)						strcpy(pBufDes, pBufSrc)
 #define STAT 										stat
+#define FOPEN(pFile, pFileName, pModel)                         (pFile = fopen(pFileName, pModel))
 #endif
 
 #endif
